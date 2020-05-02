14d80198ad1689cfa55f45dca85ac830803e7c89
Jonathan Ong <jonathanrichardong@gmail.com>
2020-05-02 13:30:57 -0700

    error testing

--- __tests__/errors.js
+++ __tests__/errors.js
@@ -1,5 +1,5 @@
 
-// const { PassThrough } = require('stream')
+const { PassThrough } = require('stream')
 const assert = require('assert')
 
 const { streamToString } = require('../utils')
@@ -25,4 +25,43 @@ test('a sync function error should be caught and thrown in the stream', async ()
   expect(result).toMatchSnapshot()
   assert.strictEqual(errors.length, 1)
   assert.strictEqual(errors[0].message, 'boom')
+})
+
+test('a stream error should be forwarded', async () => {
+  const src = new PassThrough()
+
+  const errors = []
+  const stream = render`1${src}3`
+  stream.on('error', err => errors.push(err))
+
+  setImmediate(() => {
+    src.write('2')
+    src.emit('error', new Error('boom'))
+    src.destroy()
+  })
+
+  const result = await streamToString(stream, false)
+  assert.strictEqual(result, '123')
+  expect(result).toMatchSnapshot()
+  assert.strictEqual(errors.length, 1)
+  assert.strictEqual(errors[0].message, 'boom')
+})
+
+test('should throw if an object stream is passed', async () => {
+  const src = new PassThrough({ objectMode: true })
+
+  const errors = []
+  const stream = render`1${src}3`
+  stream.on('error', err => errors.push(err))
+
+  setImmediate(() => {
+    src.write('2')
+    src.end()
+  })
+
+  const result = await streamToString(stream, false)
+  assert.strictEqual(result, '13')
+  expect(result).toMatchSnapshot()
+  assert.strictEqual(errors.length, 1)
+  assert(/object/i.test(errors[0].message))
 })
